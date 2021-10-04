85c2054917442150362f9f8f497438e79c80534f
bashiryousufy <bashiryousufy@gmail.com>
2021-10-04 18:18:00 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/simple_news_app_using_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simple_news_app_using_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:simple_news_app_using_provider/views/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
